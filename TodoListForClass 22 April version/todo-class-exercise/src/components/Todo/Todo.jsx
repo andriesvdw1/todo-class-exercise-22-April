b935e5089a2d47ec7762029d1a834d348ef31eff
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-22 16:32:56 +0200

    use a useEffect to provide initial todo items. Display each todo item in the todo list.

--- TodoListForClass 22 April version/todo-class-exercise/src/components/Todo/Todo.jsx
+++ TodoListForClass 22 April version/todo-class-exercise/src/components/Todo/Todo.jsx
@@ -3,8 +3,8 @@ import './Todo.css'
 function Todo({name, description}) {
   return (
     <div className='todo'>
-        <span>{name}</span><br />
-        <span>{description}</span>
+        <div className='todo-attribute'>{name}</div><br />
+        <div className='todo-attribute'>{description}</div> <br />
         <input className='delete-btn' type="button" value="Delete" />
         <input className='edit-btn' type="button" value="Edit" />
         <input className='done-btn' type="button" value="Done" />
--- TodoListForClass 22 April version/todo-class-exercise/src/components/TodoList/TodoList.jsx
+++ TodoListForClass 22 April version/todo-class-exercise/src/components/TodoList/TodoList.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Todo from '../Todo/Todo'
+
 function TodoList() {
+  const [todoitems, setTodoitems] = useState([]);
+
+  useEffect(() => {
+    setTodoitems([
+      { name: "potatoes", description: "boil the potatoes until soft, then mash em. Serve on the plate next to other food." },
+      { name: "cucumber", description: "Slice and add to the salad" },
+      { name: "lettuce", description: "Wash with cold water, then add to the salad" },
+      { name: "Onion", description: "Dice one onion and put on the stove pan at medium heat" },
+      { name: "Brown onion soup", description: "Mix one packet of brown onion soup with 250ml boiling water, then add to the onions in the pan." },
+    ]);
+  }, []); 
+
   return (
     <div className='todo-list'>
-        <h2>Todo List:</h2>
-        <Todo/>
+      <h2>Todo List:</h2>
+      {todoitems.map((item, index) => (
+        <Todo key={index} name={item.name} description={item.description} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
